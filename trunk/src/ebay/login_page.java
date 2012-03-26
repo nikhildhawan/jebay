85eeb4306da1ab2d8893eb4906bfbc8384fa65dc
mohitchawda@gmail.com <mohitchawda@gmail.com@ef7ddaf0-f8d1-d4f2-f8c3-b11b11dc0348>
2012-03-26 17:32:29 +0000

    First commit for jebay

---
+++ trunk/src/ebay/login_page.java
@@ -0,0 +1,94 @@
+
+
+
+package ebay;
+
+/**
+ *
+ * @author mohit
+ */
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ActionContext;
+import java.util.*;
+import java.sql.*;
+
+public class login_page  extends ActionSupport{
+
+private String uid;
+private String password;
+
+ public String getUid()
+    {
+    return uid;
+}
+
+public void setUid(String uid)
+    {
+    this.uid=uid;
+    }
+
+ public String getPassword()
+    {
+    return password;
+}
+
+public void setPassword(String password)
+    {
+    this.password=password;
+    }
+
+    @Override
+    public String execute() throws Exception
+    {
+         if(getUid().equals(""))
+         {
+             this.addActionError("Required username");
+                    return ERROR;
+         }
+
+         if(getPassword().equals(""))
+         {
+             this.addActionError("Required password");
+                    return ERROR;
+         }
+
+        try{
+
+                Class.forName("org.gjt.mm.mysql.Driver");
+                Connection con=DriverManager.getConnection("jdbc:mysql://localhost/ebay","root","");
+
+                Statement stat=con.createStatement();
+                String sql;
+	        ResultSet rs=null;
+
+                sql= "select * from user_details where user_id='"+getUid()+"' and password='"+getPassword()+"'";
+                rs = stat.executeQuery(sql);
+
+                 if (rs.next())
+                 {
+                    System.out.println("in rs next");
+                    Map session=ActionContext.getContext().getSession();
+                    session.put("logged-in","true");
+                    System.out.println(getUid());
+                    session.put("User",getUid());
+                 }
+
+                else{
+                    this.addActionError("username and password doesn't match");
+                    return ERROR;
+
+                 }
+                 }
+
+
+        catch(Exception e)
+         {
+             System.out.println(e);
+         }
+         return SUCCESS;
+    }
+
+
+}
+
+
---
+++ trunk/src/ebay/sign_up.java
@@ -0,0 +1,233 @@
+
+
+package ebay;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ActionContext;
+import java.util.*;
+import java.sql.*;
+
+public class sign_up  extends ActionSupport{
+
+
+private String firstname;
+private String lastname;
+private String password;
+private String address1;
+private String address2;
+private String city;
+private String state;
+private String country;
+private String pincode;
+private String phone;
+private String email;
+private String uid;
+private String birthdate;
+private String birthyear;
+private String birthmonth;
+private String secquestion;
+private String secanswer;
+
+public String getFirstname()
+{
+    return firstname;
+
+}
+public void setFirstname(String firstname)
+{
+    this.firstname=firstname;
+}
+
+public String getLastname()
+{
+    return lastname;
+
+}
+public void setLastname(String lastname)
+{
+    this.lastname=lastname;
+}
+
+public String getPassword()
+{
+    return password;
+
+}
+public void setPassword(String password)
+{
+    this.password=password;
+}
+
+public String getAddress1()
+{
+    return address1;
+
+}
+public void setAddress1(String address1)
+{
+    this.address1=address1;
+
+}
+public String getAddress2()
+{
+    return address2;
+
+}
+public void setAddress2(String address2)
+{
+    this.address2=address2;
+
+}
+
+public String getCity()
+{
+    return city;
+
+}
+public void setCity(String city)
+{
+    this.city=city;
+}
+
+public String getState()
+{
+    return state;
+
+}
+public void setState(String state)
+{
+    this.state=state;
+}
+public String getCountry()
+{
+    return country;
+
+}
+public void setCountry(String country)
+{
+    this.country=country;
+}
+public String getPincode()
+{
+    return pincode;
+
+}
+public void setPincode(String pincode)
+{
+    this.pincode=pincode;
+}
+
+public String getPhone()
+{
+    return phone;
+
+}
+public void setPhone(String phone)
+{
+    this.phone=phone;
+}
+
+public String getEmail()
+{
+    return email;
+
+}
+public void setEmail(String email)
+{
+    this.email=email;
+}
+
+public String getUid()
+{
+    return uid;
+
+}
+public void setUid(String uid)
+{
+    System.out.println("In setter");
+    this.uid=uid;
+}
+
+public String getBirthdate()
+{
+     return birthdate;
+}
+
+public void setBirthdate(String birthdate)
+{
+       this.birthdate=birthdate;
+}
+public String getBirthmonth()
+{
+     return birthmonth;
+}
+
+public void setBirthmonth(String birthmonth)
+{
+       this.birthmonth=birthmonth;
+}
+public String getBirthyear()
+{
+     return birthyear;
+}
+
+public void setBirthyear(String birthyear)
+{
+       this.birthyear=birthyear;
+}
+public String getSecquestion()
+{
+     return secquestion;
+}
+
+public void setSecquestion(String secquestion)
+{
+     this.secquestion=secquestion;
+}
+
+public String getSecanswer()
+{
+     return secanswer;
+}
+
+public void setSecanswer(String secanswer)
+{
+    this.secanswer=secanswer;
+}
+
+    @Override
+    public String execute() throws Exception
+    {
+
+        try{
+                System.out.println("In try");
+                System.out.println("here in try");
+                Class.forName("org.gjt.mm.mysql.Driver");
+
+                Connection con=DriverManager.getConnection("jdbc:mysql://localhost/ebay","root","");
+                Statement stat=con.createStatement();
+         		String sql;
+			String sql1;
+		        ResultSet rs=null;
+                sql="insert into user_details (user_id,first_name,last_name,home_address,city,state,country,pin_code,phone_no,email,dob,password,secret_question,secret_answer) values ('"+getUid()+"','"+getFirstname()+"','"+getLastname()+"','"+getAddress1()+getAddress2()+"','"+getCity()+"','"+getState()+"','"+getCountry()+"','"+getPincode()+"','"+getPhone()+"','"+getEmail()+"','"+getBirthdate()+"-"+getBirthmonth()+"-"+getBirthyear()+"','"+getPassword()+"','"+getSecquestion()+"','"+getSecanswer()+"')";
+				System.out.println(sql);
+                                stat.executeUpdate(sql);
+
+                Map session=ActionContext.getContext().getSession();
+                session.put("logged-in","true");
+                session.put("User",getFirstname());
+                System.out.println(getLastname());
+
+
+          }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return SUCCESS;
+    }
+
+}
+
+
+
