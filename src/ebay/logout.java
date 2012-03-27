0af52f93b8bc6f1d2541e0d80ae046fb4ed8825c
mohitchawda@gmail.com <mohitchawda@gmail.com@ef7ddaf0-f8d1-d4f2-f8c3-b11b11dc0348>
2012-03-27 05:11:49 +0000

    added logout

---
+++ src/ebay/logout.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ebay;
+
+/**
+ *
+ * @author samrat
+ */
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ActionContext;
+import java.util.*;
+import java.sql.*;
+
+public class logout extends ActionSupport
+{
+
+	@Override
+	public String execute() throws Exception
+	{
+		String user = "";
+		Map session = ActionContext.getContext().getSession();
+		if (session.containsKey("User"))
+			user = (session.get("User").toString());
+		session.remove("User");
+		return SUCCESS;
+	}
+}
