2806dad44eaef120ebbb1f301686f311a294696b
mohitchawda@gmail.com <mohitchawda@gmail.com@ef7ddaf0-f8d1-d4f2-f8c3-b11b11dc0348>
2012-04-01 20:06:20 +0000

    js for header and logout

---
+++ WebContent/js/category.js
@@ -0,0 +1,17 @@
+function getBrowseCategoriesData(){return {"items": [{"id":"11450","value":"Clothing & Accessories","url":"http://apparel.ebay.in"}
+,{"id":"267","value":"Books & Magazines","url":"http://books.ebay.in"}
+,{"id":"625","value":"Cameras & Optics","url":"http://cameras.ebay.in"}
+,{"id":"9800","value":"Cars & Bikes","url":"http://motors.ebay.in"}
+,{"id":"160","value":"Laptops & Computer Peripherals","url":"http://computers.ebay.in"}
+,{"id":"15032","value":"Mobile Phones","url":"http://mobiles.ebay.in"}
+
+
+]};}function getBrowseCatsDAPData(){return {"items": [{"id":"11450","value":"Clothing & Accessories","url":"http://apparel.shop.ebay.in"}
+,{"id":"267","value":"Books & Magazines","url":"http://books.shop.ebay.in"}
+,{"id":"625","value":"Cameras & Optics","url":"http://cameras.shop.ebay.in"}
+,{"id":"9800","value":"Cars & Bikes","url":"http://motors.shop.ebay.in"}
+,{"id":"160","value":"Laptops & Computer Peripherals","url":"http://computers.shop.ebay.in"}
+,{"id":"15032","value":"Mobile Phones","url":"http://mobiles.shop.ebay.in"}
+
+
+]};}
