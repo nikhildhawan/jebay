2528ce302c4c400d8fdb49388f2b32706909feed
mohitchawda@gmail.com <mohitchawda@gmail.com@ef7ddaf0-f8d1-d4f2-f8c3-b11b11dc0348>
2012-04-06 14:12:40 +0000

    rashi tere liye

--- trunk/WebContent/js/category.js
+++ trunk/WebContent/js/category.js
@@ -1,6 +1,6 @@
 function getBrowseCategoriesData(){return {"items": [{"id":"11450","value":"Clothing & Accessories","url":"http://apparel.ebay.in"}
 ,{"id":"267","value":"Books & Magazines","url":"http://books.ebay.in"}
-,{"id":"625","value":"Cameras & Optics","url":"confirmationsuccess.jsp"}
+,{"id":"625","value":"Cameras & Optics","url":"footer.jsp"}
 ,{"id":"9800","value":"Cars & Bikes","url":"http://motors.ebay.in"}
 ,{"id":"160","value":"Laptops & Computer Peripherals","url":"http://computers.ebay.in"}
 ,{"id":"15032","value":"Mobile Phones","url":"http://mobiles.ebay.in"}
@@ -8,7 +8,7 @@ function getBrowseCategoriesData(){return {"items": [{"id":"11450","value":"Clot
 
 ]};}function getBrowseCatsDAPData(){return {"items": [{"id":"11450","value":"Clothing & Accessories","url":"http://apparel.shop.ebay.in"}
 ,{"id":"267","value":"Books & Magazines","url":"http://books.shop.ebay.in"}
-,{"id":"625","value":"Cameras & Optics","url":"http://cameras.shop.ebay.in"}
+,{"id":"625","value":"Cameras & Optics","url":"footer.jsp"}
 ,{"id":"9800","value":"Cars & Bikes","url":"http://motors.shop.ebay.in"}
 ,{"id":"160","value":"Laptops & Computer Peripherals","url":"http://computers.shop.ebay.in"}
 ,{"id":"15032","value":"Mobile Phones","url":"http://mobiles.shop.ebay.in"}
