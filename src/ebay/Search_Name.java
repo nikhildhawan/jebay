cc8f73f9ca66ebbbad41c7bb8bd8376ace26d034
rashi.bindra@gmail.com <rashi.bindra@gmail.com@ef7ddaf0-f8d1-d4f2-f8c3-b11b11dc0348>
2012-04-07 18:05:32 +0000

    Searching in textbox

---
+++ src/ebay/Search_Name.java
@@ -0,0 +1,99 @@
+package ebay;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import model.Category_Camera;
+import model.Category_Book;
+import model.ItemDetails;
+import myutil.DB;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Search_Name extends ActionSupport{
+	private String _nkw;
+	private String _sacat;
+	private ArrayList searchDetails = new ArrayList();
+	private ArrayList colNames = new ArrayList();
+	
+	
+
+	public ArrayList getSearchDetails() {
+		return searchDetails;
+	}
+
+
+	public void setSearchDetails(ArrayList searchDetails) {
+		this.searchDetails = searchDetails;
+	}
+
+
+	public String get_nkw() {
+		return _nkw;
+	}
+
+
+	public void set_nkw(String _nkw) {
+		this._nkw = _nkw;
+	}
+
+	public String get_sacat() {
+		return _sacat;
+	}
+
+
+	public void set_sacat(String _sacat) {
+		this._sacat = _sacat;
+	}
+
+
+	public String execute(){
+		System.out.println(""+_nkw);
+		System.out.println(""+_sacat);
+		if(_sacat.equals("See-All-Categories"))
+		{
+
+			searchDetails = ItemDetails.getAllCategoryDetails(_nkw);
+			Map session = ActionContext.getContext().getSession();
+			String pdtMode =(String)session.get("pdtMode");
+			return "success";
+		}
+		else
+		{
+			searchDetails = ItemDetails.getDetails(_nkw,_sacat);
+			Map session = ActionContext.getContext().getSession();
+			String pdtMode =(String)session.get("pdtMode");
+			return "success";
+		}
+		//System.out.println(""+searchDetails.get(0));
+		/*if(_nkw.equalsIgnoreCase("camera") || _nkw.equalsIgnoreCase("cameras"))
+		{
+			searchDetails = Category_Camera.getCameraDetails(_nkw,_sacat);
+			//return "success";
+		}
+		if(_nkw.equalsIgnoreCase("book") || _nkw.equalsIgnoreCase("books"))
+		{
+			colNames = Category_Book.getCols();
+			System.out.println(""+colNames.get(1));
+			searchDetails = Category_Book.getBookDetails(_nkw);
+			//return "books";w	
+		}*/
+		/*if(_nkw.equalsIgnoreCase("computer") || _nkw.equalsIgnoreCase("computers"))
+			searchDetails = Category.getComputerDetails(_nkw);
+		if(_nkw.equalsIgnoreCase("clothing") || _nkw.equalsIgnoreCase("clothings"))
+			searchDetails = Category.getClothingDetails(_nkw);
+		if(_nkw.equalsIgnoreCase("jewellery") || _nkw.equalsIgnoreCase("jewelleries"))
+			searchDetails = Category.getJewelleryDetails(_nkw);
+		if(_nkw.equalsIgnoreCase("electric") || _nkw.equalsIgnoreCase("electrics"))
+			searchDetails = Category.getElectricDetails(_nkw);
+		if(_nkw.equalsIgnoreCase("mobile") || _nkw.equalsIgnoreCase("mobiles"))
+				searchDetails = Category.getMobileDetails(_nkw);
+		if(_nkw.equalsIgnoreCase("storage device") || _nkw.equalsIgnoreCase("storage devices"))
+			searchDetails = Category.getStorageDetails(_nkw);*/
+		
+	}
+	
+	
+	
+}
