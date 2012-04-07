762563cde1be3a784f38f06e4e0d1749fa64ba83
mohitchawda@gmail.com <mohitchawda@gmail.com@ef7ddaf0-f8d1-d4f2-f8c3-b11b11dc0348>
2012-04-07 05:19:53 +0000

    myebay summary added

--- trunk/src/ebay/Myebay.java
+++ trunk/src/ebay/Myebay.java
@@ -1,6 +1,37 @@
 package ebay;
 
+import model.*;
+import vo.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+import java.sql.*;
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionContext;
+
 public class Myebay
 {
+	ArrayList<PurchasehistoryVo> pur = new ArrayList<PurchasehistoryVo>();
+
+	public ArrayList<PurchasehistoryVo> getPur()
+	{
+		return pur;
+	}
+
+	public void setPur(ArrayList<PurchasehistoryVo> pur)
+	{
+		this.pur = pur;
+	}
+
+	public String execute()
+	{
+
+		Map session = ActionContext.getContext().getSession();
+		String username = session.get("username").toString();
+		PurchaseHistory p = new PurchaseHistory();
+		pur = p.gethistory(username);
+		return "success";
+	}
 
 }
