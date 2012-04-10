af48f303e4d793f618dc11001a778ed5556b28a5
mohitchawda@gmail.com <mohitchawda@gmail.com@ef7ddaf0-f8d1-d4f2-f8c3-b11b11dc0348>
2012-04-10 08:46:40 +0000

    myebay bidding history and sell history added

---
+++ trunk/src/model/SellHistoryMo.java
@@ -0,0 +1,42 @@
+package model;
+
+import model.*;
+import vo.*;
+
+import java.util.*;
+import ebay.*;
+
+import java.sql.*;
+
+public class SellHistoryMo
+{
+	public ArrayList<PurchasehistoryVo> sellhis = new ArrayList<PurchasehistoryVo>();
+
+	public ArrayList<PurchasehistoryVo> gethistory(String username)
+	{
+		try
+		{
+			Connect c = new Connect();
+			ResultSet rs = c.getResult("select * from transaction_details,item_details where t_item_id=item_id and t_seller='" + username + "'");
+			while (rs.next())
+			{
+				PurchasehistoryVo i = new PurchasehistoryVo();
+				i.setItem_id(rs.getInt("item_id"));
+				i.setItem_name(rs.getString("item_name"));
+				i.setItem_price(rs.getInt("item_price"));
+				i.setT_buyer(rs.getString("t_buyer"));
+				i.setT_timestamp(rs.getString("t_timestamp"));
+				sellhis.add(i);
+
+			}
+			return sellhis;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+}
