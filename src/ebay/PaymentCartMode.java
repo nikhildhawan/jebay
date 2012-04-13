59c37c4aecdfde2f6eca64f8286cba2b6ed8076a
shreepriyadogra@gmail.com <shreepriyadogra@gmail.com@ef7ddaf0-f8d1-d4f2-f8c3-b11b11dc0348>
2012-04-13 17:15:27 +0000

    Payment with cart module

---
+++ src/ebay/PaymentCartMode.java
@@ -0,0 +1,82 @@
+package ebay;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import model.BankDetails;
+import model.ItemDetails;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class PaymentCartMode extends ActionSupport{
+
+	private int item_id;
+	private int quantity;
+	private String paymentmode;
+	private String banks;
+	
+	public String getBanks() {
+		return banks;
+	}
+	public void setBanks(String banks) {
+		this.banks = banks;
+	}
+	public String getPaymentmode() {
+		return paymentmode;
+	}
+	public void setPaymentmode(String paymentmode) {
+		this.paymentmode = paymentmode;
+	}
+	public int getItem_id() {
+		return item_id;
+	}
+	public void setItem_id(int item_id) {
+		this.item_id = item_id;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public String execute()
+	{
+		String id = new Integer(item_id).toString();
+		String quant = new Integer(quantity).toString();
+		Map session=ActionContext.getContext().getSession();
+		session.put("item_id", id);
+		session.put("qty", quant);
+		System.out.println("item id is"+item_id);
+		System.out.println("mode is"+paymentmode);
+		System.out.println("Qty is"+quantity);
+		
+		String seller = ItemDetails.getSellerItem(item_id);
+		System.out.println("seller is "+seller);
+		
+		
+		if(paymentmode.equals("1"))
+		{
+			return "credit";
+		}
+		else
+		{
+			if(paymentmode.equals("2"))
+			{
+				return "debit";
+			}
+			
+			else{
+				//banks = new ArrayList();
+				
+		    	session.put("bank",banks);
+		    	
+		    	System.out.println("banks is "+banks);
+				return "bank";
+			}
+				
+		}
+	}
+}
---
+++ src/model/TransactionCart.java
@@ -0,0 +1,159 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
+
+import vo.ItemVo;
+
+import ebay.Connect;
+
+import myutil.DB;
+
+public class TransactionCart {
+
+	public static int makePayment(int price, String buyer,int item_id,int qty,int acc)
+	{
+		
+		int trans_status=0;
+		Map session=ActionContext.getContext().getSession();
+    	String cart_id_string=(String)session.get("cart_id");
+    	int cart_id = Integer.parseInt(cart_id_string);
+    	System.out.println("cart_id_string"+cart_id_string);
+		trans_status= deductAmount(acc, price);
+		
+		if(trans_status == 1)
+		{
+		String seller = ItemDetails.getSellerItem(item_id);
+		int transaction_id = getTransactionId(price, buyer, item_id, qty, acc,seller);
+		System.out.println(transaction_id);
+		reduceQuantity(item_id,qty);
+		addToPaisaPay(price, seller, buyer, transaction_id);
+		removeFromCart(cart_id);
+		
+		}
+		
+		return trans_status;
+		
+	}
+	
+	
+	
+	public static int getTransactionId(int price, String buyer,int item_id,int qty,int acc, String seller)
+	{
+
+		String sqlQuery;
+		Connection conn;
+		Statement stmt;
+		ResultSet rs = null;
+		int newGeneratedItemid = -1;
+		conn = DB.getConnection();
+		sqlQuery = "insert into transaction_details " +
+				"(t_buyer,t_seller,t_status,t_item_id,t_quantity,t_timestamp,t_buyer_account_no) " +
+				"values ('"+buyer+"','"+seller+"',1,"+item_id+","+qty+",now(),"+acc+")";
+		System.out.println(sqlQuery);
+		try
+		{
+			stmt = conn.createStatement();
+
+			System.out.println(sqlQuery);
+			stmt.executeUpdate(sqlQuery, Statement.RETURN_GENERATED_KEYS);
+			rs = stmt.getGeneratedKeys();
+			if (rs.next())
+			{
+				newGeneratedItemid = rs.getInt(1);
+				System.out.println("New generated Item_id is " + newGeneratedItemid);
+
+			}
+			else
+			{
+				System.out.println("no key generated");
+			}
+		}
+		catch (Exception ex)
+		{
+			System.out.println("no key generated");
+			ex.getStackTrace();
+		}
+		finally
+		{
+			DB.close(rs);
+			DB.close(conn);
+		}
+
+		return newGeneratedItemid;
+
+	}
+		
+	public static void removeFromCart(int cart_id){
+		String sqlQuery = "delete from cart_details where cart_id="+cart_id;
+		DB.update(sqlQuery);
+	}
+	
+	public static void reduceQuantity(int item_id, int quan)
+	{
+		ResultSet rs = null;
+		int qty=0;
+		String sqlQuery = "select item_quantity from item_details where item_id="+item_id;
+		rs = DB.readFromDB(sqlQuery);
+		try
+		{
+			if (rs.next())
+			{
+				qty=rs.getInt("item_quantity");
+				qty=qty-quan;
+			}
+		}
+		catch(Exception e){}	
+		finally{
+			String sqlQuery1 = "update item_details set item_quantity="+qty+" where item_id="+item_id;
+			DB.update(sqlQuery1);
+		}
+	}
+	
+	public static int deductAmount(int acc, int price)
+	{
+		ResultSet rs = null;
+		int balance=0;
+		int trans_status=0;
+		String sqlQuery = "select account_balance from account_details where account_no="+acc;
+		rs = DB.readFromDB(sqlQuery);
+		try
+		{
+			if (rs.next())
+			{
+				balance=rs.getInt("account_balance");
+				if(balance>price)
+				{
+				balance=balance-price;
+				trans_status=1;
+				}
+			
+			else 
+			{		
+				trans_status=0;	
+			}
+			}
+		}
+		catch(Exception e){}	
+		finally{
+			String sqlQuery1 = "update account_details set account_balance="+balance+" where account_no="+acc;
+			DB.update(sqlQuery1);
+		}
+		
+		return trans_status;
+	}
+	
+	public static void addToPaisaPay(int price,String seller,String buyer, int transaction_id)
+	{
+		String sqlQuery = "insert into `paisapay_details` " +
+				"(`paisa_seller`,`paisa_buyer`,`paisa_amount`,`paisa_t_id`) " +
+				"values ('"+seller+"','"+buyer+"',"+price+","+transaction_id+")";
+		
+		DB.update(sqlQuery);
+		
+	}
+}
